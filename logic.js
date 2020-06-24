5f9a1e6c33ae272859894d8509d0eb1df2dff640
Michael Cui <cuimichael500@gmail.com>
2020-06-24 10:40:08 -0500

    Equal button shows the two numbers

--- logic.js
+++ logic.js
@@ -12,6 +12,11 @@ $(".btn-primary").on("click",function(){
         //Append number to the first display
         $("#first-number").append(number);
 
+        //Save the number
+        firstNumber = $("#first-number").text();
+
+        
+
         }
 
         if(indicator != 0){
@@ -19,13 +24,18 @@ $(".btn-primary").on("click",function(){
         //Append number to the second display
         $("#second-number").append(number);
 
+        //Save the number
+        secondNumber = $("#second-number").text();
+
+        
+
         }
 
 });
     
 
 
-// //Operator Button Clicked
+////Operator Button Clicked
 $(".btn-danger").on("click",function(){
 
     //Change indicator so that the next number will display on second line
@@ -36,5 +46,22 @@ $(".btn-danger").on("click",function(){
 
     //Display operator text 
     $("#operator").text(operator);
+
+});
+
+////Equal
+$(".btn-success").on("click",function(){
+
+    
+
+    //Get operator text
+    var firstNumber = $("#first-number").text();
+    var secondNumber = $("#second-number").text();
+    var finalOperator = $("#operator").text();
+
+    //Display operator text 
+    console.log(firstNumber);
+    console.log(secondNumber);
+    console.log(finalOperator);
 
 });
