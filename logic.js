a37874c21923ecd1b1472c0378d211e89a57b3bb
Michael Cui <cuimichael500@gmail.com>
2020-06-24 09:44:52 -0500

    Fixed unneeded code

--- logic.js
+++ logic.js
@@ -4,12 +4,9 @@ var indicator = 0;
 //Number button Clicked
 $(".btn-primary").on("click",function(){
 
-    //get the id of the corresponding button
-    var attribute = this.id;
-
-    //Get the ID value which is the number
-    var number = $("#"+attribute).val();
-
+    //get the text of that button
+    var number = $(this).text();
+    
         if(indicator === 0){
 
         //Append number to the first display
@@ -28,45 +25,16 @@ $(".btn-primary").on("click",function(){
     
 
 
-//Operator Button Clicked
+// //Operator Button Clicked
 $(".btn-danger").on("click",function(){
 
+    //Change indicator so that the next number will display on second line
     indicator = 1; 
 
-    //get the id of the corresponding button
-    var attribute = this.id;
-    
-    //Get the ID value which is the operator
-    var operator = $("#"+attribute).val();
+    //Get operator text
+    var operator = $(this).text();
 
-        if(operator === "times"){
+    //Display operator text 
+    $("#operator").text(operator);
 
-            $("#operator").text("x");
-
-        }
-    
-        if(operator === "plus"){
-
-            $("#operator").text("+");
-            
-        }
-    
-        if(operator === "minus"){
-
-            $("#operator").text("-");
-            
-        }
-    
-        if(operator === "power"){
-
-            $("#operator").text("^");
-            
-        }
-
-        if(operator === "divide"){
-
-            $("#operator").text("/");  
-            
-        }
 });
-
